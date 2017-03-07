a363d8e17580d7a50afe33883f80cbc1003b8bf3
Riki Rusli <Riki Rusli>
2017-03-07 17:28:57 +0700

    updated button take picture dr teammate

--- src/pages/teammate-photos/teammate-photos.ts
+++ src/pages/teammate-photos/teammate-photos.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController} from 'ionic-angular';
+import {Camera} from 'ionic-native';
 
 /*
   Generated class for the TeammatePhotos page.
@@ -12,11 +13,43 @@ import { NavController} from 'ionic-angular';
   templateUrl: 'teammate-photos.html'
 })
 export class TeammatePhotosPage {
+	
+	public base64Image:string;
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController) {
+  	this.base64Image="https://placehold.it/150x150";
+
+  }
 
   ionViewDidLoad() {
     console.log('Hello TeammatePhotos Page');
   }
 
+  takePicture(){
+  	Camera.getPicture({
+  			quality : 75,
+            destinationType : Camera.DestinationType.DATA_URL,
+            sourceType : Camera.PictureSourceType.CAMERA,
+            allowEdit : true,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 300,
+            targetHeight: 300,
+            saveToPhotoAlbum: false
+
+  	}).then(imageData=>{
+  		this.base64Image="data:image/jpeg;base64,"+imageData;
+  	},error=>{
+  		console.log("Error"+JSON.stringify(error));
+  	});
+  }
+
+/*
+  getPicture(option).then((imageData)=>{
+  	let baseImage= 'data:image/jpeg;base64,'+imageData;
+
+  },(err)=>{
+
+  	//Handle error
+  })
+*/
 }
