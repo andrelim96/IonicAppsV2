45497678d6a9007b96b777b63cecd4322a6f6e4b
Mirza Lazuardi <mirzalazuardi@Gunawans-MacBook-Pro-2.local>
2017-03-08 06:21:02 +0700

    Styling update + pull teammate photo's from service

--- src/pages/teammate-photos/teammate-photos.ts
+++ src/pages/teammate-photos/teammate-photos.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController} from 'ionic-angular';
+import { Camera } from 'ionic-native';
+import { JsonTeammatePage } from '../../providers/json-teammate-page';
 
 /*
   Generated class for the TeammatePhotos page.
@@ -9,14 +11,29 @@ import { NavController} from 'ionic-angular';
 */
 @Component({
   selector: 'page-teammate-photos',
-  templateUrl: 'teammate-photos.html'
+  templateUrl: 'teammate-photos.html',
+  providers: [JsonTeammatePage]
 })
 export class TeammatePhotosPage {
+  public base64Image: String;
+  photos: any;
 
-  constructor(public navCtrl: NavController) {}
-
-  ionViewDidLoad() {
-    console.log('Hello TeammatePhotos Page');
+  constructor(public navCtrl: NavController, public data: JsonTeammatePage) {
+    this.data.load().then(result => {
+      this.photos = result;
+    });
   }
 
+  takePicture(){
+    Camera.getPicture({
+        destinationType: Camera.DestinationType.DATA_URL,
+        targetWidth: 1000,
+        targetHeight: 1000
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+        console.log(err);
+    });
+  }
 }
